60e514ecae0507125b22cc0270a4afebf28e6df0
Shubham Krishna shinde <118288116+shubhamSS9@users.noreply.github.com>
2024-05-15 06:52:38 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,40 @@
+# d1 = {"A":"Shubham", "B":"Krishna", "C":"Shinde"}
+# print(d1["A"])
+
+# courses = {"DAA":"cs", "AOA": "ME", "SVY":"CE"}
+
+# for k, v in courses.items():
+    # print(k, v)
+
+# for k in courses.keys():
+#     print(k)
+
+# for k in courses:
+    # print(k)
+
+# for v in courses.values():
+#     print(v)
+
+
+
+# courses = {"DAA":"cs", "AOA": "ME", "SVY":"CE"}
+
+# courses["SSS"] = "Web Services"
+
+# courses["AOA"] = "DSA WITH PYTHON"
+# print(courses)
+
+# del(courses["AOA"])
+# print(courses )
+
+# lst = [12, 13, 14, 15, 16]
+
+# d = dict.fromkeys(lst, 25)
+# print(d)
+
+# animal = {"tiger": 141, "lion": 152, "Leopard":110}
+
+# birds = {"Eagle":38, "Crow": 3, "Parrot": 2}
+
+# combined = {*animal, *birds}
+# print(combined)
